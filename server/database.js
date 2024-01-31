ff9eefdca0334ea440e51c43f5f7b294719f933c
Hari Om <omhari1472@gmail.com>
2024-01-31 14:53:04 +0530

    config path setup

--- server/database.js
+++ server/database.js
@@ -1,8 +1,12 @@
 import mysql from 'mysql2'
 
 import dotenv from 'dotenv';
+import { config_path } from './secret/configpath.js';
+
 // Load environment variables from .env file
-dotenv.config({ path: '/Users/hariom/Desktop/Eventor/server/secret/.env' });
+dotenv.config({
+  path:config_path,
+});
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
